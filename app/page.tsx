ec9c5f33c48837cfc610b49bd726e48d9f3edcd6
zua <farhan.javed006@gmail.com>
2025-05-24 16:29:57 +0530

    v0

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,32 @@
 'use client';
 import useMousePosition from './utils/useMousePosition';
 import styles from './page.module.scss';
-import { motion } from 'framer-motion';
+import { motion } from 'motion/react';
+import { useState } from 'react';
 
 export default function Home() {
-
-  const {x,y} = useMousePosition;
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 400 : 40;
 
   return (
     <main className={styles.main}>
-
-      <motion.div className={styles.mask}>
-        <p>
+      <motion.div
+        className={styles.mask}
+        animate={{
+          WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: 'tween', ease: 'backOut' }}
+      >
+        <p
+          onMouseEnter={() => {
+            setIsHovered(true);
+          }}
+          onMouseLeave={() => {
+            setIsHovered(false);
+          }}
+        >
           A visual designer - with skills that haven't been replaced by A.I (yet) - making good shit
           only if the paycheck is equally good.
         </p>
@@ -23,7 +38,6 @@ export default function Home() {
           high quality & impactful digital experience.
         </p>
       </div>
-
     </main>
   );
 }
